d9f3e58a38274828fa4cded3b2c9e9a980e6969b
GoodOldJack12 <goodoldjack12@gmail.com>
2026-03-15 16:58:27 +0100

    feat: add support for GS1 barcodes

--- GrocyScanner.Core/Validators/GtinValidtor.cs
+++ GrocyScanner.Core/Validators/GtinValidtor.cs
@@ -4,10 +4,12 @@ namespace GrocyScanner.Core.Validators;
 
 public class GtinValidator : IGtinValidator
 {
-    private static Regex _gtinRegex = new Regex("^(\\d{8}|\\d{12,14})$");
+    private static Regex _gtinRegex = new Regex("^(\\d{8}|\\d{12,14}|010\\d{13})$");
     
     public bool Validate(string barcode)
     {
+        barcode = ParseBarcode(barcode);
+        
         if (!_gtinRegex.IsMatch(barcode))
         {
             return false;
@@ -16,5 +18,16 @@ public class GtinValidator : IGtinValidator
         barcode = barcode.PadLeft(14, '0');
         int sum = barcode.Select((c,i) => (c - '0')  * (i % 2 == 0 ? 3 : 1)).Sum();
         return sum % 10 == 0;
+    }
+
+    public string ParseBarcode(string barcode)
+    {
+        // Parse GS1 barcode and return EAN13
+        if (barcode.Length >= 16 && barcode.StartsWith("01"))
+        {
+            return barcode.Substring(3,13);
+        }
+
+        return barcode;
     }
 }
--- GrocyScanner.Core/Validators/IGtinValidator.cs
+++ GrocyScanner.Core/Validators/IGtinValidator.cs
@@ -3,4 +3,6 @@ namespace GrocyScanner.Core.Validators;
 public interface IGtinValidator
 {
     public bool Validate(string barcode);
+
+    public string ParseBarcode(string barcode);
 }
